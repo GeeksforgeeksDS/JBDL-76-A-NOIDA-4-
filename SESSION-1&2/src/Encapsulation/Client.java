026a284d5c0894d655f300f2cff85fc496f764b3
GeeksforgeeksDS <courses@geeksforgeeks.org>
2024-09-18 14:02:34 +0530

    Update Client.java

--- SESSION-1&2/src/Encapsulation/Client.java
+++ SESSION-1&2/src/Encapsulation/Client.java
@@ -10,15 +10,9 @@ public class Client {
         person.setId(18);
         person.setName("java 8");
         person.setAddress("amazon");
-
-
         System.out.println(person.getAddress());
         System.out.println(person.getId());
         System.out.println(person.getName());
-
-
-
-
 
     }
 }
