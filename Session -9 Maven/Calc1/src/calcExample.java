e476e196d851a22a7b588a33068dcdca74d92b35
GeeksforgeeksDS <courses@geeksforgeeks.org>
2024-10-13 17:30:39 +0530

    Add files via upload

---
+++ Session -9 Maven/Calc1/src/calcExample.java
@@ -0,0 +1,20 @@
+public class calcExample {
+   // project p1
+    public  static int add(int a, int b){
+        return a+b;
+    }
+
+      public  static int sub(int a, int b){
+        return a-b;
+    }
+
+      public  static int multiply(int a, int b){
+        return a*b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello java ");
+    }
+
+
+}
---
+++ Session -9 Maven/calcMaven/src/main/java/in/geeksforgeeks/AddCalc.java
@@ -0,0 +1,9 @@
+package in.geeksforgeeks;
+
+public class AddCalc {
+
+    public  static  int add(int a, int b){
+        return a+b;
+    }
+
+}
